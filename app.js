6c3cf415d645101477da768130bc7bf91a1d702f
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-08-10 00:04:34 +0200

    Ur virker og vises kun en gang på hjemmeside. Der kan arbejdes videre med det og gøres pænere og udvælge relevante dele til brug.

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 "use strict";
 
+let currentTime = [];
+
 window.addEventListener("load", start);
 
 function start() {
   console.log("JS kører");
   const date = new Date();
-    console.log(date);
-    const timeAndDateNow = /*html*/
-        `${date}`;
+  console.log(date);
+  currentTime.push(date);
+  console.log(currentTime);
+  showTimeMove();
+  showTime(currentTime);
+  currentTime.pop();
+  console.log(currentTime);
+}
+
+function showTime(timeList) {
+  document.querySelector("#clock").innerHTML = "";
+  for (const time of timeList) {
+    const timeAndDateNow = /*html*/ `${time}`;
     document.querySelector("#clock").insertAdjacentHTML("beforeend", timeAndDateNow);
+  }
+}
+
+function showTimeMove() {
+  const callStartEverySecond = setTimeout(start, 1000);
 }
